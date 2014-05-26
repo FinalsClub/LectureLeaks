87c0856553d7fa0974fe055926c7db6b23791d9c
Bryan Bonvallet <btbonval@gmail.com>
2014-05-26 12:42:09 -0400

    workspace changes left on LectureLeaks.org main system for years, no idea who is responsible

--- unishare/settings.py
+++ unishare/settings.py
@@ -98,6 +98,7 @@ INSTALLED_APPS = (
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
+     'gunicorn',
      'tagging',
      'captcha',
      'unishare.documents',
